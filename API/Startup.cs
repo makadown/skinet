99306107f515f646874f45afea7bdd15d5e5f3f7
makadown <maseflores79@gmail.com>
2021-06-21 19:00:48 -0500

    Adding a not found endopint error handler

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,12 +39,16 @@ namespace API {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
+        {
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
                 app.UseSwagger ();
                 app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
             }
+
+            // This redirects to our ErrorController!
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection ();
 
