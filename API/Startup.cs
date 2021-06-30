af3cd837c0a65ea4b453013ea2dca416e8dec914
makadown <maseflores79@gmail.com>
2021-06-29 19:24:38 -0500

    Adding CORS Suppor to the api

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors( opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +52,7 @@ namespace API
             app.UseRouting();
             // THIS IS VERY IMPORTANT so you can use wwwroot to serve static files
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
