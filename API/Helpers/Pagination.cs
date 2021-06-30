adf8a51461ea9491a024264897da3163be27f78d
makadown <maseflores79@gmail.com>
2021-06-29 19:16:39 -0500

    Adding search functionality

--- API/Helpers/Pagination.cs
+++ API/Helpers/Pagination.cs
@@ -4,16 +4,16 @@ namespace API.Helpers
 {
     public class Pagination<T> where T : class
     {
-        public Pagination(int pageIndex, int page, int count, IReadOnlyList<T> data)
+        public Pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> data)
         {
             PageIndex = pageIndex;
-            Page = page;
+            PageSize = pageSize;
             Count = count;
             Data = data;
         }
 
         public int PageIndex { get; set; }
-        public int Page { get; set; }
+        public int PageSize { get; set; }
         public int Count { get; set; }
         public IReadOnlyList<T> Data { get; set; }
     }
