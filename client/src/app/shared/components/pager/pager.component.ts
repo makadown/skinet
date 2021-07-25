e961231049d740d5d61e48c3fd0bfd195569c362
makadown <maseflores79@gmail.com>
2021-07-25 15:24:58 -0500

    Pagination and output properties

---
+++ client/src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss'],
+})
+export class PagerComponent implements OnInit {
+  @Input() totalCount = 0;
+  @Input() pageSize = 0;
+  @Output() pageChanged = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onPagerChanged(e: any) {
+    this.pageChanged.emit(e.page);
+  }
+}
