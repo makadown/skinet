eabbeccdcd715da9d5307046630beb3f17fcf939
makadown <maseflores79@gmail.com>
2021-07-27 05:15:46 -0500

    Setting up nav links

--- client/src/app/about/about.module.ts
+++ client/src/app/about/about.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    AboutComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [HomeComponent]
+  exports: [AboutComponent]
 })
-export class HomeModule { }
+export class AboutModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { ShopModule } from './shop/shop.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    ShopModule,
+    HomeModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
