408f4558bddbb9153bccc7a3deb3c4ec487fc61e
makadown <maseflores79@gmail.com>
2022-05-14 12:56:24 -0700

    adding new register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,5 +44,26 @@ namespace API.Controllers
                 DisplayName = user.UserName
             };
         }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser 
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+
+            return new UserDto {
+                DisplayName = user.DisplayName,
+                Token = "This will be a token, baby!",
+                Email = user.Email
+            };
+        }
     }
 }
